2daee92023aa69cbe770d7ab9054e6ca6f6c7397
cooltaby <cooltaby@users.noreply.github.com>
2019-12-30 13:36:16 +0530

    Add files via upload

---
+++ offline.js
@@ -0,0 +1,23 @@
+﻿{
+	"version": 1577693204,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-3.4.1.min.js",
+		"offlineClient.js",
+		"images/coin-sheet0.png",
+		"images/coin-sheet1.png",
+		"images/head-sheet0.png",
+		"images/tail-sheet0.png",
+		"images/pushtoflip-sheet0.png",
+		"images/head2-sheet0.png",
+		"images/tail2-sheet0.png",
+		"images/sprite-default-000.jpg",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
